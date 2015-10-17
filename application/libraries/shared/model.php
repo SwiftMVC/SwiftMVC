99dbf9cb4391af317f14f87e57339e978288fa9f
Faizan Ayubi <indianayubi@gmail.com>
2015-10-17 22:28:58 +0530

    Removed setDeleted

--- application/libraries/shared/model.php
+++ application/libraries/shared/model.php
@@ -48,7 +48,6 @@ namespace Shared {
             $raw = $primary["raw"];
             if (empty($this-> $raw)) {
                 $this->setCreated(date("Y-m-d H:i:s"));
-                $this->setDeleted(false);
                 $this->setLive(true);
             }
             $this->setModified(date("Y-m-d H:i:s"));
