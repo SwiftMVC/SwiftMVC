4c3b1fd79f018aaee67d80c2269292d2d0abfd95
Hemant Mann <hemant.mann121@gmail.com>
2017-01-02 12:44:39 +0530

    Fixed error when using Framework with nginx

--- index.php
+++ index.php
@@ -22,6 +22,18 @@ try {
         }
     });
 
+    if (!function_exists('getallheaders')) { 
+        function getallheaders() { 
+            $headers = ''; 
+            foreach ($_SERVER as $name => $value) { 
+               if (substr($name, 0, 5) == 'HTTP_') { 
+                   $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value; 
+               }
+           }
+           return $headers;
+        }
+    }
+
     // 2. load the Core class that includes an autoloader
     require("framework/core.php");
     Framework\Core::initialize();
